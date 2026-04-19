09ba74a92aac81e49a7e7a3c064308e5b7437622
an-calren1 <adam@calren.io>
2026-04-18 20:53:15 -0400

    fix: add database role init script for first startup

---
+++ db-init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  CREATE USER rackvio_app WITH PASSWORD '${APP_DB_PASS}';
+  CREATE USER rackvio_migrations WITH PASSWORD '${MIGRATIONS_DB_PASS}';
+
+  ALTER DATABASE rackvio OWNER TO rackvio_migrations;
+  GRANT ALL PRIVILEGES ON DATABASE rackvio TO rackvio_migrations;
+  GRANT CONNECT ON DATABASE rackvio TO rackvio_app;
+
+  \c rackvio
+
+  ALTER DEFAULT PRIVILEGES FOR ROLE rackvio_migrations
+    GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO rackvio_app;
+
+  ALTER DEFAULT PRIVILEGES FOR ROLE rackvio_migrations
+    GRANT USAGE, SELECT ON SEQUENCES TO rackvio_app;
+
+  GRANT USAGE ON SCHEMA public TO rackvio_app;
+EOSQL
